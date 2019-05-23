caf4527fb133344ec4c0fa5e41941dafb5a8c793
adann0 <40341834+adann0@users.noreply.github.com>
2019-05-22 21:39:44 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# ensure initial pharo image
-if ([ "$PHARO_IMAGE" = "Pharo.image" ] && [ ! -e "$PHARO_HOME/Pharo.image" ]) ; then
-    cp /usr/local/bin/pharo/Pharo*.* $PHARO_HOME
-fi
-
 # supervisor log file
-mkdir -p $PHARO_HOME/logs && touch $PHARO_HOME/logs/$PHARO_SUPERVISOR_LOG_NAME
+mkdir -p $SQUEAK_HOME/logs && touch $SQUEAK_HOME/logs/$SQUEAK_SUPERVISOR_LOG_NAME
 
 # vnc
 /bin/bash /dockerstartup/vnc_startup.sh &
